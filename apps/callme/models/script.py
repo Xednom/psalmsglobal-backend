8c9cb2bade84e323ec905e8e4f6888667788849b
Xednom <monde.lacanlalay@gmail.com>
2021-06-12 15:58:38 +0800

    added django-mysql for listfield

--- apps/callme/models/script.py
+++ apps/callme/models/script.py
@@ -1,23 +1,41 @@
 from django.db import models
-
+from django.db.models.fields import CharField
+from django_mysql.models import ListTextField
 from apps.core.models import TimeStamped
 
 
-__all__ = ("AttributeType", "Form", "Script")
+__all__ = ("AttributeDataType", "Attribute", "Form", "Script")
 
 
-class AttributeType(TimeStamped):
-    text = models.CharField(max_length=500, blank=True)
-    question = models.TextField(blank=True)
+class AttributeDataType(models.TextChoices):
+    text = "text", ("Text")
+    question = "question", ("Question")
 
 
 class Form(TimeStamped):
     form_title = models.CharField(max_length=250)
-    attribute = models.ForeignKey(
-        AttributeType, related_name="form_attributes", on_delete=models.CASCADE
+    company = models.ForeignKey(
+        "callme.Company",
+        related_name="company_forms",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
     )
-    value_text = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return f"{self.form_title}"
+
+
+class Attribute(TimeStamped):
+    form = models.ForeignKey(
+        Form, related_name="attribute_forms", on_delete=models.CASCADE
+    )
+    data_type = models.CharField(choices=AttributeDataType.choices, max_length=20)
+    value_text = models.CharField(max_length=500, blank=True)
     value_question = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.form}"
 
 
 class Script(TimeStamped):
@@ -28,11 +46,18 @@ class Script(TimeStamped):
     form = models.ForeignKey(
         Form, related_name="form_scripts", on_delete=models.CASCADE
     )
-    mailing_lists = models.CharField(max_length=500)
+    mailing_lists = ListTextField(
+        base_field=models.CharField(max_length=500),
+        size=100
+    )
+    status = models.BooleanField(default=True)
 
     def get_company_address(self):
-        return self.company.company_complete_address
+        if self.company:
+            return self.company.company_complete_address
+        else:
+            return ""
 
     def save(self, *args, **kwargs):
         self.company_address = self.get_company_address()
-        super(self, Script).save(*args, **kwargs)
+        super(Script, self).save(*args, **kwargs)
