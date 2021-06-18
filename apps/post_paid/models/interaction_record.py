ee37d9e5ec0628f69ebf6fe5d23a38a8a6d58907
Xednom <monde.lacanlalay@gmail.com>
2021-06-18 22:05:38 +0800

    added ticket number and customer interaction post paid in interaction record

--- apps/post_paid/models/interaction_record.py
+++ apps/post_paid/models/interaction_record.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from apps.core.models import TimeStamped
+from apps.post_paid.models import CustomerInteractionPostPaid
 
 
 __all__ = ("InteractionRecord",)
@@ -8,15 +9,27 @@ __all__ = ("InteractionRecord",)
 
 class InteractionRecord(TimeStamped):
     date_called = models.DateField(auto_now_add=True)
+    ticket_number = models.CharField(max_length=250, blank=True)
+    customer_interaction_post_paid = models.ForeignKey(
+        CustomerInteractionPostPaid,
+        related_name="customer_interaction_post_paid_records",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
     client = models.ForeignKey(
         "authentication.Client",
         related_name="post_paid_client_interaction_records",
-        on_delete=models.DO_NOTHING,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     agent = models.ForeignKey(
         "authentication.Staff",
         related_name="post_paid_agent_interaction_records",
-        on_delete=models.DO_NOTHING,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
     )
     total_minutes = models.IntegerField()
     summary = models.TextField(blank=True)
