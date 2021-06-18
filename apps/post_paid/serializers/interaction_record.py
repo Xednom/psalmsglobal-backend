dc57eb2eaa923f60d6a1c88cf7b18d0dc6b4176b
Xednom <monde.lacanlalay@gmail.com>
2021-06-18 22:06:14 +0800

    added new fields in serializer for interaction record

--- apps/post_paid/serializers/interaction_record.py
+++ apps/post_paid/serializers/interaction_record.py
@@ -4,7 +4,7 @@ from apps.post_paid.models import InteractionRecord
 from apps.authentication.models import Client, Staff
 
 
-__all__ = ("AccountChargeSerializer",)
+__all__ = ("InteractionRecordSerializer",)
 
 
 class InteractionRecordSerializer(serializers.ModelSerializer):
@@ -14,13 +14,25 @@ class InteractionRecordSerializer(serializers.ModelSerializer):
     agent = serializers.PrimaryKeyRelatedField(
         queryset=Staff.objects.all(), required=False, allow_null=True
     )
+    agent_name = serializers.SerializerMethodField()
+    agent_code = serializers.SerializerMethodField()
 
     class Meta:
         model = InteractionRecord
         fields = (
             "client",
             "agent",
+            "agent_code",
+            "agent_name",
             "date_called",
+            "ticket_number",
             "total_minutes",
             "summary",
+            "customer_interaction_post_paid",
         )
+    
+    def get_agent_name(self, instance):
+        return f"{instance.agent.staff_name}"
+    
+    def get_agent_code(self, instance):
+        return f"{instance.agent.staff_id}"
