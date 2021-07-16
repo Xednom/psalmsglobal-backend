df326d7a3d7fad1b2c0955b1036c819037a14035
Xednom <monde.lacanlalay@gmail.com>
2021-07-16 18:04:11 +0800

    fixed filtering for minutes report

--- apps/post_paid/views/minutes_report.py
+++ apps/post_paid/views/minutes_report.py
@@ -19,12 +19,8 @@ class MinutesReportViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         current_user = self.request.user
         user = User.objects.filter(username=current_user)
-        if (
-            current_user.designation_category == "current_client"
-            or current_user.designation_category == "new_client"
-            or current_user.designation_category == "affiliate_partner"
-        ):
-            qs = MinutesReport.objects.select_related("client").filter(client=user)
+        if current_user:
+            qs = MinutesReport.objects.select_related("client").filter(client__user__in=user)
             return qs
         elif current_user.is_superuser:
             qs = MinutesReport.objects.all()
