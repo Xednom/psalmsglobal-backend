061946d56ac9d837c779ac8ba0a632d322b131d0
Xednom <monde.lacanlalay@gmail.com>
2021-08-31 12:08:38 +0800

    added delete command for plan summary and payment

---
+++ apps/post_paid/management/commands/delete_payment_summary.py
@@ -0,0 +1,53 @@
+import calendar
+import datetime
+
+from decimal import Decimal
+from datetime import date
+
+from django.db.models import Sum, Q, Case, When, DecimalField
+from django.core.management.base import BaseCommand
+
+from post_office.models import EmailTemplate
+from post_office import mail
+
+from apps.authentication.models import Client
+from apps.post_paid.models import (
+    AccountBalance,
+    MonthlyCharge,
+    InteractionRecord,
+    JobOrderPostPaid,
+    PostPaid,
+)
+
+
+class Command(BaseCommand):
+    help = """Automatically create Plan Summary and Payment for every user in the system monthly.
+        Note: this will only work if there is a Plan detail(Postpaid) for the Client.
+        """
+
+    def handle(self, *args, **kwargs):
+
+        today = datetime.datetime.now()
+        month_year = str(calendar.month_abbr[today.month]) + "/" + str(today.year)
+
+        client_name = (
+            PostPaid.objects.select_related("client")
+            .all()
+            .values_list("client", flat=True)
+            .distinct()
+        )
+        client = Client.objects.filter(id__in=client_name)
+
+        for i in client:
+            client_monthly_charge = MonthlyCharge.objects.filter(
+                client=i, month_year=month_year
+            ).exists()
+            client_post_paid = PostPaid.objects.select_related(
+                "client", "plan_type"
+            ).filter(client=i, recurring_bill=True, account_status=True)
+            for post_paid in client_post_paid:
+                if post_paid:
+                    if client_monthly_charge:
+                        MonthlyCharge.objects.filter(
+                            client=i, month_year=month_year, payment_status=False
+                        ).delete()
