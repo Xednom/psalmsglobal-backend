4ba2452b0062f3e05457063ba6b0fb108b4b35ea
Xednom <monde.lacanlalay@gmail.com>
2021-09-15 19:34:10 +0800

    added the newsfeed in the admin side

--- apps/newsfeed/admin.py
+++ apps/newsfeed/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from apps.newsfeed.models import NewsFeed, NewsFeedComment
+
+
+class NewsFeedCommentAdmin(admin.TabularInline):
+    model = NewsFeedComment
+    extra = 1
+    readonly_fields = ("created_at", "user")
+
+
+class NewsFeedAdmin(admin.ModelAdmin):
+    model = NewsFeed
+    list_display = ("title", "body", "publish_to")
+    inlines = [NewsFeedCommentAdmin]
+    
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for obj in formset.deleted_objects:
+            obj.delete()
+        for instance in instances:
+            instance.user = request.user
+            instance.save()
+        formset.save_m2m()
+
+
+admin.site.register(NewsFeed, NewsFeedAdmin)
