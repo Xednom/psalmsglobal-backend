748e0cbca5097ad1c5f6fe6dff8c88e7eabc739a
Xednom <monde.lacanlalay@gmail.com>
2021-11-08 20:45:11 +0800

    updated the url for callme redundant list in show_urls

--- apps/callme/urls.py
+++ apps/callme/urls.py
@@ -21,23 +21,23 @@ from apps.callme.views import (
 
 router = routers.DefaultRouter()
 
-router.register(r"company", CompanyViewSet, basename="company-list")
-router.register(r"crm", CrmViewSet, basename="crm-list")
-router.register(r"phone-system", PhoneSystemViewSet, basename="phone-system-list")
+router.register(r"company", CompanyViewSet, basename="company")
+router.register(r"crm", CrmViewSet, basename="crm")
+router.register(r"phone-system", PhoneSystemViewSet, basename="phone-system")
 router.register(r"vodaconnect-plan", VodaconnectPlanViewSet, basename="vodaconnect-plan")
 router.register(r"vodaconnect-line-type", VodaconnectLineTypeViewSet, basename="vodaconnect-line-type")
-router.register(r"script", ScriptViewSet, basename="script-list")
-router.register(r"form", FormViewSet, basename="form-list")
-router.register(r"state", StateViewSet, basename="state-list")
-router.register(r"county", CountyViewSet, basename="county-list")
-router.register(r"callme-info", CallMeInfoViewSet, basename="callme-info-list")
-router.register(r"offer-status", OfferStatusViewSet, basename="offer-status-list")
-router.register(r"property-info", PropertyInfoViewSet, basename="property-info-list")
+router.register(r"script", ScriptViewSet, basename="script")
+router.register(r"form", FormViewSet, basename="form")
+router.register(r"state", StateViewSet, basename="state")
+router.register(r"county", CountyViewSet, basename="county")
+router.register(r"callme-info", CallMeInfoViewSet, basename="callme-info")
+router.register(r"offer-status", OfferStatusViewSet, basename="offer-status")
+router.register(r"property-info", PropertyInfoViewSet, basename="property-info")
 
 app_name = "callme"
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("callme/", include(router.urls), name="callme"),
     path("file-upload/", FileUploadView.as_view(), name="file-upload"),
     path("interaction-form/<int:id>/", FormView.as_view(), name="form-view")
 ]
