69a41a3afd0e8536c1f889555880910406e64df4
Xednom <monde.lacanlalay@gmail.com>
2021-11-09 17:33:19 +0800

    added client name in forwarding information serializer

--- apps/vodaconnect/serializers/subscribers_inventory.py
+++ apps/vodaconnect/serializers/subscribers_inventory.py
@@ -152,6 +152,7 @@ class ForwardingInformationSerializer(serializers.ModelSerializer):
     client = serializers.PrimaryKeyRelatedField(
         queryset=Client.objects.all(), required=False, allow_null=True
     )
+    client_name = serializers.CharField(source="client.user.user_full_name", read_only=True)
     forwarding_information_total_number_of_extensions = (
         TotalNumberOfExtensionSerializer(many=True, required=False, allow_null=True)
     )
@@ -167,6 +168,7 @@ class ForwardingInformationSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "client",
+            "client_name",
             "forwarding_number",
             "notes",
             "forwarding_information_total_number_of_extensions",
