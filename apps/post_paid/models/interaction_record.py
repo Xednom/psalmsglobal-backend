c074564230dcd49ec2fdeedd403990eb12311685
Xednom <monde.lacanlalay@gmail.com>
2023-05-15 21:30:29 +0800

    #9 added ticket summary record

--- apps/post_paid/models/interaction_record.py
+++ apps/post_paid/models/interaction_record.py
@@ -97,3 +97,72 @@ class InteractionRecord(TimeStamped):
     def save(self, *args, **kwargs):
         self.client = self.get_customer_interaction()
         super(InteractionRecord, self).save(*args, **kwargs)
+
+
+class TicketSummaryRecord(TimeStamped):
+    date_called = models.DateField(auto_now_add=True)
+    ticket_number = models.CharField(max_length=250, blank=True)
+    ticket_summary = models.ForeignKey(
+        "post_paid.TicketSummary",
+        related_name="ticket_summary_records",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    client = models.ForeignKey(
+        "authentication.Client",
+        related_name="client_ticket_summary_records",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    agent = models.ForeignKey(
+        "authentication.Staff",
+        related_name="agent_ticket_summary_records",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    total_minutes = models.DecimalField(
+        max_digits=19, decimal_places=2, default=0.00, blank=True, null=True
+    )
+    summary = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=50,
+        choices=StatusChoices.choices,
+        default=StatusChoices.approved,
+        blank=True,
+    )
+    client_feedback_status = models.CharField(
+        max_length=100,
+        choices=FeedBackStatus.choices,
+        default=FeedBackStatus.na,
+        blank=True,
+    )
+    dispute_details = models.TextField(blank=True)
+    other_feedback = models.TextField(blank=True)
+    client_notes = models.TextField(blank=True)
+    internal_management_ticket_status = models.CharField(
+        max_length=100,
+        choices=TicketStatus.choices,
+        default=TicketStatus.na,
+        blank=True,
+    )
+    memo_solution_from_the_mgmt = models.TextField(blank=True)
+    other_ticket_status = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["-client"]
+        verbose_name = "Ticket Summary Record Breakdown"
+        verbose_name_plural = "Ticket Summary Record Breakdown"
+
+    def __str__(self):
+        return self.ticket_number
+
+    def get_client_interaction(self):
+        client = self.ticket_summary.company.client
+        return client
+
+    def save(self, *args, **kwargs):
+        self.client = self.get_client_interaction()
+        super(TicketSummaryRecord, self).save(*args, **kwargs)
