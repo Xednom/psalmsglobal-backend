1a31748d635e19a5d5420b683c66d94370c95aba
Xednom <monde.lacanlalay@gmail.com>
2023-06-17 16:47:59 +0800

    #22 added in init.py comment classes

--- apps/callme/models/__init__.py
+++ apps/callme/models/__init__.py
@@ -3,4 +3,11 @@ from .crm import Crm  # noqa
 from .phone_system import PhoneSystem, VodaconnectLineType, VodaconnectPlan  # noqa
 from .script import Script, Attribute, AttributeDataType, Form  # noqa
 from .geography import State, County  # noqa
-from .callme_info import OfferStatus, PropertyInfo, PropertyFileInfo  # noqa
+from .callme_info import (
+    OfferStatus,
+    PropertyInfo,
+    PropertyFileInfo,
+    CommentOfferTabAgent,
+    CommentOfferTabClient,
+    CommentOfferTabCustomer,
+)  # noqa
